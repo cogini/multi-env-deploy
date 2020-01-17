230819bb88aabbf77c78b838eeb452a5125b6fe5
Jake Morrison <jake@cogini.com>
2020-01-17 20:14:06 +0800

    Doc

--- terraform/modules/asg/main.tf
+++ terraform/modules/asg/main.tf
@@ -12,7 +12,7 @@
 
 # Example config:
 # terraform {
-#   source = "${get_terragrunt_dir()}/../../../modules//asg-app"
+#   source = "${get_terragrunt_dir()}/../../../modules//asg"
 # }
 # dependency "vpc" {
 #   config_path = "../vpc"
@@ -31,26 +31,30 @@
 # inputs = {
 #   comp = "app"
 #
-#   min_size = 1
+#   min_size = 0
 #   max_size = 3
 #   desired_capacity = 1
+#
 #   wait_for_capacity_timeout = "2m"
+#   # Wait for this number of healthy instances in load balancer
+#   wait_for_elb_capacity = 1
 #
 #   health_check_grace_period = 30
-#
-#   # Respond to client requests via load balancer
 #   health_check_type = "ELB"
-#   wait_for_elb_capacity = 1
-#   target_group_arns = [dependency.tg.outputs.arn]
 #
-#   # Worker
+#   # wait_for_capacity_timeout = "0"
 #   # health_check_type = "EC2"
+#
+#   target_group_arns = [dependency.tg.outputs.arn]
 #
 #   subnets = dependency.vpc.outputs.subnets["private"]
 #
 #   launch_template_id = dependency.lt.outputs.launch_template_id
 #   launch_template_version = "$Latest" # $Latest, or $Default
-#   # spot_max_price = ""
+#   spot_max_price = ""
+#   on_demand_base_capacity = 0
+#   on_demand_percentage_above_base_capacity = 0
+#   override_instance_types = ["t3.nano", "t3a.nano"]
 #
 #   force_delete = true
 # }
