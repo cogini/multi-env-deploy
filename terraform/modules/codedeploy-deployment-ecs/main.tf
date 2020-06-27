a46486ecb8f000d083d5e70e2cd9a6a29e5443bc
Jake Morrison <jake@cogini.com>
2020-06-27 08:58:50 +0800

    Support ECS

--- terraform/modules/codedeploy-deployment-ecs/main.tf
+++ terraform/modules/codedeploy-deployment-ecs/main.tf
@@ -4,58 +4,90 @@ locals {
   name = var.name == "" ? "${var.app_name}-${var.comp}-ecs" : var.name
   trigger_name = var.trigger_name == "" ? local.name : var.trigger_name
   deploy_hook = var.deploy_hook == "" ? local.name : var.deploy_hook
-  deployment_group_name = var.deployment_group_name == "" ? var.name : var.deployment_group_name
 }
-
-
 
 # https://www.terraform.io/docs/providers/aws/r/codedeploy_deployment_group.html
 resource "aws_codedeploy_deployment_group" "this" {
-  app_name              = var.codedeploy_app_name
-  deployment_group_name = local.deployment_group_name
-  service_role_arn      = var.codedeploy_service_role_arn
+  app_name                = var.codedeploy_app_name
+  deployment_group_name   = local.name
+  deployment_config_name  = var.deployment_config_name
+  service_role_arn        = var.codedeploy_service_role_arn
 
-  deployment_config_name = var.deployment_config_name
+  dynamic "ecs_service" {
+    for_each = var.ecs_service_name == null ? [] : list(1)
+    content {
+      cluster_name = var.ecs_cluster_name
+      service_name = var.ecs_service_name
+    }
+  }
 
-  ecs_service {
-    cluster_name = aws_ecs_cluster.example.name
-    service_name = aws_ecs_service.example.name
+  # ASG
+  dynamic "load_balancer_info" {
+    for_each = var.target_group_name == null ? [] : list(1)
+    content {
+      target_group_info {
+        name = var.target_group_name
+      }
+    }
+  }
+
+  # ECS
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/create-blue-green.html
+  # https://docs.aws.amazon.com/codedeploy/latest/userguide/deployment-groups-create-load-balancer-for-ecs.html
+  dynamic "load_balancer_info" {
+    for_each = length(var.target_group_names) > 0 ? list(1) : []
+    content {
+      target_group_pair_info {
+        prod_traffic_route {
+          listener_arns = var.listener_arns
+        }
+
+        dynamic "target_group" {
+          for_each = var.target_group_names
+          iterator = target_group_name
+          content {
+            name = target_group_name.value
+          }
+        }
+      }
+    }
   }
 
   deployment_style {
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-    deployment_type   = "BLUE_GREEN"
+    deployment_option = var.deployment_option
+    deployment_type   = var.deployment_type
   }
 
-  blue_green_deployment_config {
-    deployment_ready_option {
-      action_on_timeout    = var.deployment_ready_option_action_on_timeout
-      wait_time_in_minutes = var.deployment_ready_option_wait_time_in_minutes
-    }
+  dynamic "blue_green_deployment_config" {
+    for_each = var.deployment_type == "BLUE_GREEN" ? list(1) : []
+    content {
+      deployment_ready_option {
+        action_on_timeout    = var.deployment_ready_option_action_on_timeout
+        wait_time_in_minutes = var.deployment_ready_option_wait_time_in_minutes
+      }
 
-    terminate_blue_instances_on_deployment_success {
-      action                           = "TERMINATE"
-      termination_wait_time_in_minutes = var.termination_wait_time_in_minutes
+      dynamic "green_fleet_provisioning_option" {
+        for_each = var.provisioning_action == null ? [] : list(1)
+        content {
+          action = var.provisioning_action
+        }
+      }
+
+      terminate_blue_instances_on_deployment_success {
+        action                           = var.termination_action
+        termination_wait_time_in_minutes = var.termination_wait_time_in_minutes
+      }
     }
   }
 
+  # Rollback when deployment fails or monitoring threshold is met
   auto_rollback_configuration {
     enabled = var.auto_rollback_configuration_enabled
     events  = var.auto_rollback_configuration_events
   }
 
-  load_balancer_info {
-    target_group_info {
-      name = var.target_group_name
-    }
-  }
-
-  trigger_configuration {
-    trigger_name       = local.trigger_name
-    trigger_events     = var.trigger_events
-    trigger_target_arn = var.trigger_target_arn
-  }
-
+  # Stop deployment when CloudWatch alarm detects that a metric has fallen
+  # below or exceeded a defined threshold
   dynamic "alarm_configuration" {
     for_each = var.alarm_configuration
     content {
@@ -63,5 +95,11 @@ resource "aws_codedeploy_deployment_group" "this" {
       enabled                   = lookup(alarm_configuration.value, "enabled", null)
       ignore_poll_alarm_failure = lookup(alarm_configuration.value, "ignore_poll_alarm_failure", null)
     }
+  }
+
+  trigger_configuration {
+    trigger_name       = local.trigger_name
+    trigger_events     = var.trigger_events
+    trigger_target_arn = var.trigger_target_arn
   }
 }
--- terraform/modules/codedeploy-deployment-ecs/variables.tf
+++ terraform/modules/codedeploy-deployment-ecs/variables.tf
@@ -3,37 +3,8 @@ variable "comp" {
 }
 
 variable "name" {
-  description = "Name, var.app_name-var.comp if blank"
+  description = "Name, var.app_name-var.comp-ecs if blank"
   default = ""
-}
-
-variable "codedeploy_service_role_arn" {
-  description = "CodeDeploy IAM service role"
-}
-
-variable "codedeploy_app_name" {
-  description = "CodeDeploy app name"
-}
-
-variable "deployment_group_name" {
-  description = "Name of the deployment group, var.name if blank"
-  default     = ""
-}
-
-variable "trigger_name" {
-  description = "Name of trigger, var.name if blank"
-  default     = ""
-}
-
-variable "trigger_target_arn" {
-  description = "Trigger SNS topic"
-  default     = ""
-}
-
-variable "trigger_events" {
-  description = "DeploymentStart, DeploymentSuccess, DeploymentFailure, DeploymentStop, DeploymentRollback, InstanceStart, InstanceSuccess, InstanceFailure."
-  default     = ["DeploymentFailure"]
-  type        = list(string)
 }
 
 variable "deploy_hook" {
@@ -43,11 +14,86 @@ variable "deploy_hook" {
 
 variable "target_group_name" {
   description = "Target group name"
+  type        = string
+  default     = null
+}
+
+# ECS
+variable "ecs_cluster_name" {
+  description = "ECS cluster name"
+  default = null
+}
+
+variable "ecs_service_name" {
+  description = "ECS service name"
+  type        = string
+  default     = null
+}
+
+variable "target_group_names" {
+  description = "ECS target group names"
+  type        = list(string)
+  default     = []
+}
+
+variable "listener_arns" {
+  description = "ECS load balancer listener ARNs"
+  type        = list(string)
+  default     = []
+}
+
+variable "codedeploy_app_name" {
+  description = "CodeDeploy app name"
+  type        = string
+}
+
+variable "codedeploy_service_role_arn" {
+  description = "CodeDeploy IAM service role"
+}
+
+variable "deployment_type" {
+  description = "BLUE_GREEN or IN_PLACE"
+  default     = null
+}
+
+variable "deployment_option" {
+  description = "WITH_TRAFFIC_CONTROL or WITHOUT_TRAFFIC_CONTROL"
+  default     = null
 }
 
 variable "deployment_config_name" {
   description = "Deployment config name"
   default     = "CodeDeployDefault.ECSAllAtOnce"
+}
+
+# Blue/Green
+variable "deployment_ready_option_action_on_timeout" {
+  description = "When to reroute traffic from original environment to replacement environment: CONTINUE_DEPLOYMENT (auto) or STOP_DEPLOYMENT (manual)"
+  default     = "STOP_DEPLOYMENT"
+}
+
+# Blue/Green
+variable "deployment_ready_option_wait_time_in_minutes" {
+  description = "Minutes to wait before the status of a blue/green deployment changed to Stopped if rerouting is not started manually. Applies only when action is STOP_DEPLOYMENT"
+  default     = 5
+}
+
+# Blue/Green
+variable "provisioning_action" {
+  description = "Method used to add instances to a replacement environment: DISCOVER_EXISTING or COPY_AUTO_SCALING_GROUP"
+  default     = null
+}
+
+# Blue/Green
+variable "termination_action" {
+  description = "Action to take on instances in the original environment after a successful blue/green deployment: TERMINATE or KEEP_ALIVE"
+  default     = "TERMINATE"
+}
+
+# Blue/Green
+variable "termination_wait_time_in_minutes" {
+  description = "How long to wait to terminate the instances after a successful deployment"
+  default     = 5
 }
 
 # https://www.terraform.io/docs/providers/aws/r/codedeploy_deployment_group.html#auto-rollback-configuration
@@ -67,17 +113,18 @@ variable "alarm_configuration" {
   default     = []
 }
 
-variable "deployment_ready_option_action_on_timeout" {
-  description = "When to reroute traffic from an original environment to a replacement environment in a blue/green deployment"
-  default     = "STOP_DEPLOYMENT" # CONTINUE_DEPLOYMENT
+variable "trigger_name" {
+  description = "Name of trigger, var.name if blank"
+  default     = ""
 }
 
-variable "deployment_ready_option_wait_time_in_minutes" {
-  description = "Minutes to wait before the status of a blue/green deployment changed to Stopped if rerouting is not started manually. Applies only when action is STOP_DEPLOYMENT"
-  default     = 5
+variable "trigger_target_arn" {
+  description = "Trigger SNS topic"
+  default     = ""
 }
 
-variable "termination_wait_time_in_minutes" {
-  description = "How long to wait to terminate the instances after a successful deployment"
-  default     = 5
+variable "trigger_events" {
+  description = "DeploymentStart, DeploymentSuccess, DeploymentFailure, DeploymentStop, DeploymentRollback, InstanceStart, InstanceSuccess, InstanceFailure."
+  default     = ["DeploymentFailure"]
+  type        = list(string)
 }
