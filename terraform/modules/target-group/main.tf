acb56287fabd1eed43aef91a9b83194776feaeb6
Jake Morrison <jake@cogini.com>
2020-06-27 08:50:44 +0800

    Make more configurable

--- terraform/modules/target-group/main.tf
+++ terraform/modules/target-group/main.tf
@@ -19,16 +19,18 @@
 # }
 #
 # inputs = {
+#   host_name = "app"
+#
 #   port = 4001
 #   protocol = "HTTPS"
 #
 #   health_check = {
 #     path = "/"
-#     interval = 30
-#     timeout = 10
-#     healthy_threshold = 2
-#     unhealthy_threshold = 2
-#     matcher = "200"
+#     # interval = 30 # default 30
+#     # timeout = 10 # default 5
+#     healthy_threshold = 2 # default 3
+#     unhealthy_threshold = 2 # default 3
+#     matcher = 200
 #   }
 #
 #   # stickiness = {
@@ -37,11 +39,12 @@
 #
 #   listener_arn = dependency.lb.outputs.listener_arn
 #   vpc_id = dependency.vpc.outputs.vpc_id
+#   zone_name = dependency.zone.outputs.name
 # }
 
 locals {
   name  = var.name == "" ? "${var.app_name}-${var.comp}" : var.name
-  hosts = length(var.hosts) == 0 ? ["${var.host_name}.${replace(var.zone_name, "/\\.$/", "")}"] : var.hosts
+  hosts = [for host in var.hosts : replace(host, "/\\.$/", "")]
 }
 
 # https://www.terraform.io/docs/providers/aws/r/lb_target_group.html
@@ -50,28 +53,31 @@ resource "aws_lb_target_group" "this" {
   name        = local.name
   port        = var.port
   protocol    = var.protocol
+  deregistration_delay = var.deregistration_delay
 
   dynamic "health_check" {
-    for_each = [var.health_check]
+    for_each = var.health_check == null ? [] : list(1)
     content {
-      enabled             = lookup(health_check.value, "enabled", null)
-      healthy_threshold   = lookup(health_check.value, "healthy_threshold", null)
-      interval            = lookup(health_check.value, "interval", null)
-      matcher             = lookup(health_check.value, "matcher", null)
-      path                = lookup(health_check.value, "path", null)
-      port                = lookup(health_check.value, "port", null)
-      protocol            = lookup(health_check.value, "protocol", null)
-      timeout             = lookup(health_check.value, "timeout", null)
-      unhealthy_threshold = lookup(health_check.value, "unhealthy_threshold", null)
+      enabled             = lookup(var.health_check, "enabled", null)
+      healthy_threshold   = lookup(var.health_check, "healthy_threshold", null)
+      interval            = lookup(var.health_check, "interval", null)
+      matcher             = lookup(var.health_check, "matcher", null)
+      path                = lookup(var.health_check, "path", null)
+      port                = lookup(var.health_check, "port", null)
+      protocol            = lookup(var.health_check, "protocol", null)
+      timeout             = lookup(var.health_check, "timeout", null)
+      unhealthy_threshold = lookup(var.health_check, "unhealthy_threshold", null)
     }
   }
 
+  # slow_start
+
   dynamic "stickiness" {
-    for_each = [var.stickiness]
+    for_each = var.stickiness == null ? [] : list(1)
     content {
-      cookie_duration = lookup(stickiness.value, "cookie_duration", null)
-      enabled         = lookup(stickiness.value, "enabled", null)
-      type            = stickiness.value.type
+      cookie_duration = lookup(var.stickiness, "cookie_duration", null)
+      enabled         = lookup(var.stickiness, "enabled", null)
+      type            = lookup(var.stickiness, "type", "lb_cookie")
     }
   }
 
@@ -98,22 +104,31 @@ resource "aws_lb_target_group" "this" {
 # https://www.terraform.io/docs/providers/aws/r/lb_listener_rule.html
 # https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-update-rules.html
 resource "aws_lb_listener_rule" "this" {
-  listener_arn = var.listner_arn
+  count = var.listener_rule ? 1 : 0
+  listener_arn = var.listener_arn
 
-  # priority = var.priority
+  priority = var.priority
 
   action {
     target_group_arn = aws_lb_target_group.this.arn
     type             = "forward"
   }
 
-  condition {
-    field  = "host-header"
-    values = local.hosts
+  dynamic "condition" {
+    for_each = length(local.hosts) > 0 ? list(1) : []
+    content {
+      host_header {
+        values = local.hosts
+      }
+    }
   }
 
-  condition {
-    field  = "path-pattern"
-    values = var.paths
+  dynamic "condition" {
+    for_each = length(var.paths) > 0 ? list(1) : []
+    content {
+      path_pattern {
+        values = var.paths
+      }
+    }
   }
 }
--- terraform/modules/target-group/variables.tf
+++ terraform/modules/target-group/variables.tf
@@ -18,45 +18,46 @@ variable "protocol" {
   default     = "HTTP" # HTTP | HTTPS
 }
 
+variable "deregistration_delay" {
+  description = "Draining time in secodns"
+  default     = 300
+}
+
 variable "health_check" {
   description = "Mapping of tags for target group health_check"
   type        = map
-  default     = {}
+  default     = null
 }
 
 variable "stickiness" {
   description = "Mapping of tags for target group stickiness"
-  default = {
-    type    = "lb_cookie"
-    enabled = false
-  }
+  type      = map
+  default   = null
 }
 
 variable "target_type" {
   description = "The type of target, values are instance: instance, ip, lambda"
-  default     = "instance"
-}
-
-variable "host_name" {
-  description = "Host for listener rule condition, domain will be added"
-  default     = "*"
+  # Default "instance"
+  default     = null
 }
 
 variable "hosts" {
   description = "Host for listener rule condition, domain will be added"
+  type        = list(string)
   default     = []
 }
 
 variable "paths" {
   description = "Hosts for listener rule condition, domain will be added"
   type        = list(string)
-  default     = ["/*"]
+  # default     = ["/*"]
+  default     = []
 }
 
-# variable "priority" {
-#   description = "Listener rule priority"
-#   default     = ""
-# }
+variable "priority" {
+  description = "Listener rule priority"
+  default     = null
+}
 
 variable "vpc_id" {
   description = "VPC id"
@@ -64,4 +65,9 @@ variable "vpc_id" {
 
 variable "listener_arn" {
   description = "ARN of LB listner"
+}
+
+variable "listener_rule" {
+  description = "Whether to add a listener rule"
+  default     = true
 }
