6fb4f3525b1726c3dabdd1248941c174aeeab020
Jake Morrison <jake@cogini.com>
2020-06-27 09:04:14 +0800

    Support ECS

--- terraform/modules/codedeploy/main.tf
+++ terraform/modules/codedeploy/main.tf
@@ -1,6 +1,11 @@
 # Create CodeDeploy app for component
 
+locals {
+  name = var.name == "" ? "${var.app_name}-${var.comp}" : var.name
+}
+
+# https://www.terraform.io/docs/providers/aws/r/codedeploy_app.html
 resource "aws_codedeploy_app" "this" {
+  name             = local.name
   compute_platform = var.compute_platform
-  name             = "${var.app_name}-${var.comp}"
 }
--- terraform/modules/codedeploy/variables.tf
+++ terraform/modules/codedeploy/variables.tf
@@ -2,7 +2,13 @@ variable "comp" {
   description = "Part of the app, e.g. app, worker"
 }
 
+variable "name" {
+  description = "Name, app_name-comp if blank"
+  default = ""
+}
+
 variable "compute_platform" {
   description = "Type of deployment: ECS, Lambda, or Server"
-  default     = "Server"
+  # Default is Server
+  default     = null
 }
