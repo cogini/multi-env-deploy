5e0f32d12b83c05065a9c7e589e7ce8f268e3546
Jake Morrison <jake@cogini.com>
2022-07-31 14:35:55 -0500

    Clean up

--- terraform/modules/iam-codebuild-github-action/main.tf
+++ terraform/modules/iam-codebuild-github-action/main.tf
@@ -34,21 +34,6 @@ resource "aws_iam_role" "this" {
   ]
 }
 EOF
-    # Version = "2012-10-17",
-    # Statement = [{
-    #   Action = "sts:AssumeRoleWithWebIdentity"
-    #   Effect = "Allow"
-    #   Principal = {
-    #     Federated = "arn:aws:iam::${local.account_id}:oidc-provider/token.actions.githubusercontent.com"
-    #   }
-    #   Condition = {
-    #     StringLike = {
-    #       # "token.actions.githubusercontent.com:aud" :  ["sts.amazonaws.com" ],
-    #       "token.actions.githubusercontent.com:sub" : var.sub
-    #     }
-    #   }
-    # }]
-  # })
 }
 
 # Add permissions to run CodeBuild and get resulting log messages
