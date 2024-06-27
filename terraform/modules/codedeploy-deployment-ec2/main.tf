190c0d7f000876939b6f9e0374b9115cdd63a343
Jake Morrison <jake@cogini.com>
2024-06-27 15:31:27 -0500

    Support tag sets

--- terraform/modules/codedeploy-deployment-ec2/main.tf
+++ terraform/modules/codedeploy-deployment-ec2/main.tf
@@ -13,20 +13,8 @@ resource "aws_codedeploy_deployment_group" "this" {
   deployment_config_name = var.deployment_config_name
   service_role_arn       = var.codedeploy_service_role_arn
 
-
-  # dynamic "ec2_tag_set" {
-  #   for_each = var.ec2_tag_set
-  #   content {
-  #     dynamic "ec2_tag_filter" {
-  #       for_each = lookup(ec2_tag_set.value, "ec2_tag_filter", [])
-  #       content {
-  #         key   = lookup(ec2_tag_filter.value, "key", null)
-  #         type  = lookup(ec2_tag_filter.value, "type", null)
-  #         value = lookup(ec2_tag_filter.value, "value", null)
-  #       }
-  #     }
-  #   }
-  # }
+  # https://docs.aws.amazon.com/codedeploy/latest/userguide/instances-tagging.html
+  # The Terraform names are confusing. Filters are "OR" and Sets are "AND".
 
   dynamic "ec2_tag_filter" {
     iterator = tag_filter
@@ -38,11 +26,26 @@ resource "aws_codedeploy_deployment_group" "this" {
     }
   }
 
+  dynamic "ec2_tag_set" {
+    for_each = var.ec2_tag_set
+    iterator = tag_set
+    content {
+      dynamic "ec2_tag_filter" {
+        for_each = tag_set.value
+        iterator = tag_filter
+        content {
+          key   = lookup(tag_filter.value, "key", null)
+          type  = lookup(tag_filter.value, "type", null)
+          value = lookup(tag_filter.value, "value", null)
+        }
+      }
+    }
+  }
+
   deployment_style {
     deployment_option = var.deployment_option
     deployment_type   = var.deployment_type
   }
-
 
   dynamic "on_premises_instance_tag_filter" {
     iterator = tag_filter
@@ -53,6 +56,22 @@ resource "aws_codedeploy_deployment_group" "this" {
       value = lookup(tag_filter.value, "value", null)
     }
   }
+
+  # Not supported by Terraform :-/
+  # dynamic "on_premises_tag_set" {
+  #   for_each = (length(var.on_premises_tag_set) > 0) ? [var.on_premises_tag_set] : []
+  #   content {
+  #     dynamic "on_premises_instance_tag_filter" {
+  #       for_each = var.on_premises_instance_tag_set
+  #       iterator = tag_filter
+  #       content {
+  #         key   = lookup(tag_filter.value, "key", null)
+  #         type  = lookup(tag_filter.value, "type", null)
+  #         value = lookup(tag_filter.value, "value", null)
+  #       }
+  #     }
+  #   }
+  # }
 
   # Rollback when deployment fails or monitoring threshold is met
   auto_rollback_configuration {
