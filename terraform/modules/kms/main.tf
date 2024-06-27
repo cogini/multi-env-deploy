6b3b228c1dd6339b7a3a1f7519538f2cd8c22281
Jake Morrison <jake@cogini.com>
2024-06-27 15:36:42 -0500

    Doc

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -4,10 +4,11 @@
 # terraform {
 #   source = "${dirname(find_in_parent_folders())}/modules//kms"
 # }
-# include {
+# include "root" {
 #   path = find_in_parent_folders()
 # }
 # inputs = {
+#   # Allow AWS service linked role for EC2 auto scaling to mount encrypted EBS volumes
 #   enable_ec2_as = true
 # }
 
